dd7b1286ac13cca58afda03fcc4898d386db5efc
Fermin Martin <fermartorr01@gmail.com>
2020-10-09 18:29:55 +0200

    Preparamos los templates
    1 Hacemos el .gitignore de node_modules
    2 Creamos los scripts para hacer build con babel y con webpack
    3 Creamos la página Home(Pages).js
    4 Creamos el template Header (Templates)
    5 Creamos la página Character

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, dist), 
+        path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
     },
     resolve: {
         extensions: ['.js'],
     },
+    //SETTING BABLE RULES
     module: {
         rules: [
             {
+                // SETTING REGEX FOR .JS FILES
                 test: /\.js?$/,
                 exclude: /node_modules/,
                 use: {
@@ -21,12 +23,13 @@ module.exports = {
             }
         ]
     },
+    //SETTING PLUGINS
     plugins: [
         new HtmlWebpackPlugin([
             {
-            inject: true,
-            template: './public/index.html',
-            filename: './index.html',
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html',
             }
         ])
     ]
